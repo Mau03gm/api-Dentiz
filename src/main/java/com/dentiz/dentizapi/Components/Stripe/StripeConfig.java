4c21b564feb8848564847d3064e96714638e42c4
Mau03gm <gatojs0109@outlook.com>
2024-03-18 11:42:24 -0600

    add payment intent confirm

--- src/main/java/com/dentiz/dentizapi/Components/Stripe/StripeConfig.java
+++ src/main/java/com/dentiz/dentizapi/Components/Stripe/StripeConfig.java
@@ -1,18 +1,18 @@
 package com.dentiz.dentizapi.Components.Stripe;
 
 import com.stripe.StripeClient;
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@Getter
 public class StripeConfig {
     @Value("${stripe.public-key}")
     private String publicKey;
     @Value("${stripe.secret-key}")
     private String secretKey;
 
-    @Value("${stripe.stripe-id}")
-    private String stripeId;
 
     public StripeClient getStripeClient() {
         return new StripeClient(
